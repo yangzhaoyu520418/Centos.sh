67ac1aa6d5a5d4ebc247711350660b7c18277b07
yzy520418 <2320171246@qq.com>
2018-11-12 14:33:23 +0800

    LAMP/LNMP

--- LNMP.sh
+++ LNMP.sh
@@ -2,51 +2,5 @@
 # author for yangzhaoyu 2018.11.12
 # use install LNMP scrips
 
-Nginx_script(){
-	cat << END
-----------------------------------------
-|************Nginx menu************|
-----------------------------------------
-`echo -e "\033[35m 1)Nginx installation\033[0m"`
-`echo -e "\033[35m 2) installation\033[0m"`
-`echo -e "\033[35m 3)\033[0m"`
-`echo -e "\033[35m 4)exit\033[0m"` 
-END
-}
-
-Mariadb_script(){
-	cat << END
-----------------------------------------
-|************Mariadb menu************|
-----------------------------------------
-`echo -e "\033[35m 1)Nginx installation\033[0m"`
-`echo -e "\033[35m 2) installation\033[0m"`
-`echo -e "\033[35m 3)\033[0m"`
-`echo -e "\033[35m 4)exit\033[0m"` 
-END
-}
-
-mysql_script(){
-	cat << EOF
-----------------------------------------
-|************Mysql menu************|
-----------------------------------------
-`echo -e "\033[35m 1)Nginx installation\033[0m"`
-`echo -e "\033[35m 2) installation\033[0m"`
-`echo -e "\033[35m 3)\033[0m"`
-`echo -e "\033[35m 4)exit\033[0m"`
-EOF
-}
-php_script(){
-	cat << EOF
-----------------------------------------
-|************PHP menu************|
-----------------------------------------
-`echo -e "\033[35m 1)Nginx installation\033[0m"`
-`echo -e "\033[35m 2) installation\033[0m"`
-`echo -e "\033[35m 3)\033[0m"`
-`echo -e "\033[35m 4)exit\033[0m"`
-EOF
-}
 
 
