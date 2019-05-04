c8b5658ade186f3bfdb2271042e61bf001d0c022
yzy <yzy520418@gmail.com>
2019-05-04 23:35:38 +0800

    20190504

---
+++ iptables_01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+iptables -F
+iptables -F -t nat
+iptables -X 
+iptables -Z 
+iptables -P INPUT DROP
+iptables -A INPUT -m state --state NEW -j DROP
+iptables -A INPUT -m state --state ESTABLISHED,RELAED -j ACCEPT
