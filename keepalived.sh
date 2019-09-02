6ab8bcea8f8de62b75d79726e7032836aeac540a
yzy <yzy520418@gmail.com>
2019-09-02 15:33:34 +0800

    KEEPALIVED

--- keepalived.sh
+++ keepalived.sh
@@ -28,7 +28,20 @@ service_ad(){
 install_Keepalived(){
 	local KEEPALIVED_URL="https://www.keepalived.org/software/keepalived-2.0.18.tar.gz"
 	local WGET_DIR="/usr/bin/wget" 
-	local KEEPALIVED_DIR="/soft/keepalived
-	local 
-
+	local KEEPALIVED_DIR="/soft/keepalived"
+	local KEEPALIVED_ETC="/etc/keepalived"
+	[ -f ${WGET_DIR} ] || yum -y install wget >> /dev/null
+	[ -d ${KEEPALIVED_DIR}  ] || mkdir -p ${KEEPALIVED_DIR}
+	wget -p ${KEEPALIVED_URL} ${KEEPALIVED_DIR}
+	tar xf ${KEEPALIVED_DIR}/${KEEPALIVED_URL##*/} -C ${KEEPALIVED_DIR}/
+	rm -rf ${KEEPALIVED_DIR}/${KEEPALIVED_URL##*/}
+	KEEP_DIR=`ls ${KEEPALIVED_DIR}`
+	cd ${KEEPALIVED_DIR}/${KEEP_DIR} ; ./configure --profix=/usr/local/keepalived ; make && make install 
+	[ $? -eq 0 ] && echo "The keepalive is installtion" || echo "The Keepalived is not installtion" && exit 1
 }
+
+rootNess
+yum_Ist
+service_ad
+install_Keepalived
+
